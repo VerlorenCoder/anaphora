e9bc51811daf063b72d97eceff41cba746d0734c
Jakub Batogowski <jakub.batogowski@gmail.com>
2018-12-29 13:54:17 +0100

    Implemented merging proper names

--- src/main/java/logic/impl/EnglishTaggerImpl.java
+++ src/main/java/logic/impl/EnglishTaggerImpl.java
@@ -9,6 +9,7 @@ import opennlp.tools.postag.POSTaggerME;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class EnglishTaggerImpl implements Tagger {
@@ -34,11 +35,44 @@ public class EnglishTaggerImpl implements Tagger {
         }
 
         mergeTokensWithTags(tokens, tags);
+        mergeProperNouns(tokens);
     }
 
     private void mergeTokensWithTags(List<Token> tokens, String[] tags) {
         for (int i = 0; i < tokens.size(); i++) {
             tokens.get(i).setTag(Tag.fromAbbreviation(tags[i]));
         }
+    }
+
+    private void mergeProperNouns(List<Token> tokens) {
+        for (int previousToken = 0, currentToken = 1; currentToken < tokens.size(); previousToken++, currentToken++) {
+            if (isProperNoun(tokens.get(previousToken)) && isProperNoun(tokens.get(currentToken))) {
+                merge(tokens.get(previousToken), tokens.get(currentToken), tokens);
+            }
+        }
+    }
+
+    private boolean isProperNoun(Token token) {
+        return token.getTag().equals(Tag.PROPER_NOUN_SINGULAR) || token.getTag().equals(Tag.PROPER_NOUN_PLURAL);
+    }
+
+    private void merge(Token firstToken, Token secondToken, List<Token> tokens) {
+        if (haveSameTags(firstToken, secondToken)) {
+            firstToken.setValue(mergeTokensValues(firstToken, secondToken));
+            tokens.remove(secondToken);
+        }
+        else {
+            firstToken.setValue(mergeTokensValues(firstToken, secondToken));
+            firstToken.setTag(Tag.PROPER_NOUN_PLURAL);
+            tokens.remove(secondToken);
+        }
+    }
+
+    private boolean haveSameTags(Token firstToken, Token secondToken) {
+        return firstToken.getTag().equals(secondToken.getTag());
+    }
+
+    private String mergeTokensValues(Token firstToken, Token secondToken) {
+        return firstToken.getValue() + " " + secondToken.getValue();
     }
 }
